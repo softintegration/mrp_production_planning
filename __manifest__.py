6efa5bfd8453caed448c1873c786ae00b9fe98d1
Mossaab BOUDCHICHA <boudchicha23@hotmail.fr>
2023-06-24 18:11:17 +0100

    Manufacturing planning:Add the «Validation Date» field to tree view

--- __manifest__.py
+++ __manifest__.py
@@ -10,7 +10,7 @@
  'qweb': [],
  'description': False,
  'images': [],
- 'version': '1.0.2.14',
+ 'version': '1.0.2.15',
  'category': 'Manufacturing/Manufacturing',
  'demo': [],
  'depends': ['mrp_production_request','record_scheduling'],
